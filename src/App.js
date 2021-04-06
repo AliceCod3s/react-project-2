8a6a35d1dd94a0b46e0bb6db513b1d5f255684d1
Alice <alicewrites@hotmail.com>
2021-04-06 20:45:11 +0300

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 function App() {
   
    return <div className="App">
-     Weather App
+    <h1> Weather App</h1>
+     <footer>
+        This project was coded by <a href="https://www.instagram.com/imissalice/" target="_blank">Alice Gitman </a>  and is {" "}
+     <a href="https://github.com/AliceCod3s/react-project-2" target="_blank">
+        open-sourced on GitHub
+     </a>
+     </footer>
     </div>;
 
 }
